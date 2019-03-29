cccdd6e15a94204d303dc9d7c5123ddc32f34c63
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-03-29 21:12:46 +0100

    Update widget.js

--- widget-master/js/widget.js
+++ widget-master/js/widget.js
@@ -9,7 +9,7 @@ class LeMondeWidget extends Widget {
 		this.header = true;
 		this.footer = false;
 		this.sizeX = 2;
-		this.sizeY = 0,5;
+		this.sizeY = 0.5;
 		this.radius = 15;
 	}
 	
@@ -47,28 +47,23 @@ class LeMondeView extends WidgetView {
 
 	draw() {
 		super.draw();
-		this.link = HH.create("a");
-		SS.style(this.link, {"fontSize": "10px", "textDecoration": "none"});
-		this.stage.appendChild(this.link);
-		
-		let height = (this.try.mvc.main.header ? 25 : 0) + (this.try.mvc.main.footer ? 25 : 0);
-		this.try.numberContainer = HTML.create("div");
-		CSS.style(this.try.numberContainer, {"paddingTop": height / 2 + "px", "width": "100%", "height": "calc(100% - " + height + "px)", "lineHeight": "calc(100%)", "textAlign": "center", "fontSize": "55px"});
-		this.try.numberContainer.innerHTML = 0;
-		this.try.stage.appendChild(this.try.numberContainer);
-		
-		this.try.footer.innerHTML = "refresh";
-		CSS.style(this.try.footer, {"userSelect": "none", "cursor": "pointer"});
-		Events.on(this.try.footer, "click", event => this.try.mvc.controller.refreshClick());
-		this.try.stage.appendChild(this.try.footer);
-		
+		//this.link = HH.create("a");
+		//SS.style(this.link, {"fontSize": "10px", "textDecoration": "none"});
+		//this.stage.appendChild(this.link);
+		this.bloc = HTML.create("input");
+		this.bloc.setAttribute("type", "text");
+		this.bloc.setAttribute("value", "Entrer un mot");
+		this.stage.appendChild(this.bloc);
+		//this.texte = HH.create("h3");
+		//this.texte.innerHTML = this.article;
+		//this.stage.appendChild(this.texte);
 		
 	}
 	
-	update(title, link) {
-		this.link.innerHTML = title;
-		HH.attr(this.link, {"href": "https://www.lemonde.fr" + link, "target": "_blank"});
-	}
+	//update(title, link) {
+	//	this.link.innerHTML = title;
+	//	HH.attr(this.link, {"href": "https://www.lemonde.fr" + link, "target": "_blank"});
+	//}
 	
 }
 
@@ -82,14 +77,13 @@ class LeMondeController extends WidgetController {
 		super.setUp();
 		
 	}
-	
+
 	async load() {
-		let result = await this.mvc.main.dom("https://lemonde.fr"); // load web page
+		let result = await this.mvc.main.dom("https://www.linguee.fr/francais-anglais/search?source=auto&query=bonjour"); // load web page
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		let article = new xph().doc(dom).ctx(dom).craft('//*[@id="en-continu"]/div/ul/li[1]/a').firstResult; // find interesting things
-		this.mvc.view.update(article.textContent, article.getAttribute("href"));
-	}
-	
+		//let article = new xph().doc(dom).ctx(dom).craft('//*[@id="dictEntry10000724317"]').firstResult; // find interesting things
+		//this.mvc.view.update(article.textContent, article.getAttribute("href"));
+	}	
 }
