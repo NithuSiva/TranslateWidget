2df93adeb6b00a94c66de1a6fed4677a89b008a0
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-03-30 14:15:29 +0100

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -1,15 +1,15 @@
-class LeMondeWidget extends Widget {
+class TraductionWidget extends Widget {
 	
 	constructor(id, app) {
-		super(id, LeMondeModel, LeMondeView, LeMondeController, app);
+		super(id, TraductionModel, TraductionView, TraductionController, app);
 	}
 	
 	setUp() {
 		super.setUp();
 		this.header = true;
-		this.footer = false;
+		this.footer = true;
 		this.sizeX = 2;
-		this.sizeY = 0.5;
+		this.sizeY = 1;
 		this.radius = 15;
 	}
 	
@@ -21,7 +21,7 @@ class LeMondeWidget extends Widget {
 	
 }
 
-class LeMondeModel extends WidgetModel {
+class TraductionModel extends WidgetModel {
 	
 	constructor() {
 		super();
@@ -34,7 +34,7 @@ class LeMondeModel extends WidgetModel {
 
 }
 
-class LeMondeView extends WidgetView {
+class TraductionView extends WidgetView {
 	
 	constructor() {
 		super();
@@ -47,14 +47,49 @@ class LeMondeView extends WidgetView {
 
 	draw() {
 		super.draw();
-		this.bloc = HH.create("input");
+
+		this.bloc = HH.create("input"); //creer un champ texte. id = "ChampTexte" size = "39" type = "text"
+        this.bloc.setAttribute("id", "ChampTexte");
+		this.bloc.setAttribute("size", "39");
 		this.bloc.setAttribute("type", "text");
-		this.bloc.setAttribute("value", "Entrer un mot");
 		this.stage.appendChild(this.bloc);
-		/*this.link = HH.create("a");
-		SS.style(this.link, {"fontSize": "10px", "textDecoration": "none"});
-		this.stage.appendChild(this.link);
-		*/
+
+		this.langueBase = HH.create("select"); //creer une liste deroulante pour la langue du mot. id = "langueBase"
+		this.langueBase.setAttribute("id", "langueBase");
+
+
+		this.francais1 = HH.create("option"); //contenue de la liste "langueBase"
+		this.francais1.innerHTML = "Francais";
+
+		this.langueBase.appendChild(this.francais1); 
+
+		this.anglais1 = HH.create("option"); //contenue de la liste "langueBase"
+		this.anglais1.innerHTML = "Anglais";
+
+		this.langueBase.appendChild(this.anglais1); 
+
+		this.stage.appendChild(this.langueBase); //mise en forme
+        
+        this.langueTrad = HH.create("select"); //creer une liste deroulante pour la langue du mot a traduire. id = "langueTrad"
+        this.langueTrad.setAttribute("id", "langueTrad");
+        
+        this.francais2 = HH.create("option"); //contenue de la liste "langueTrad"
+		this.francais2.innerHTML = "Francais";
+
+		this.langueTrad.appendChild(this.francais2); 
+
+		this.anglais2 = HH.create("option"); //contenue de la liste "langueTrad"
+		this.anglais2.innerHTML = "Anglais";
+
+		this.langueTrad.appendChild(this.anglais2);
+
+		this.stage.appendChild(this.langueTrad); //mise en forme
+        
+        this.footer.innerHTML = "valider";  // mise en forme du footer permettant de valider les valeur et choix entrer.
+		SS.style(this.footer, {"userSelect": "none", "cursor": "pointer"});
+		Events.on(this.footer, "click", event => this.mvc.controller.refreshClick());
+		this.stage.appendChild(this.footer);
+        
 	}
 	
 	/*update(title, link) {
@@ -65,7 +100,7 @@ class LeMondeView extends WidgetView {
 	
 }
 
-class LeMondeController extends WidgetController {
+class TraductionController extends WidgetController {
 	
 	constructor() {
 		super();
@@ -75,6 +110,19 @@ class LeMondeController extends WidgetController {
 		super.setUp();
 		
 	}
+    
+    refreshClick() {
+        this.tableauLangue = ["Francais","Anglais"]; //liste contenant les langues.
+		this.mot = document.getElementById("ChampTexte").value; // variable contenant la valeur contenue dans le champ texte.
+        this.base = document.getElementById("langueBase").selectIndex; 
+        this.baseChoix = langueBase.selectedIndex;  // variable qui contient le choix de la langue du mot
+        this.trad = document.getElementById("langueTrad").selectIndex;
+        this.tradChoix = langueTrad.selectedIndex; // variable qui contient le choix de langue dans lequel sera traduit le mot.
+        //alert("[" + this.mot + "]" + " " + this.tableauLangue[this.baseChoix] + " -->" + " " + this.tableauLangue[this.tradChoix] + " " );
+        
+        
+	}
+    
 	
 	/*async load() {
 		let result = await this.mvc.main.dom("https://lemonde.fr"); // load web page
