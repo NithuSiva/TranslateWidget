82cf1ef9579e9d5652b005331b6026d4ab880aed
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-03-30 14:20:01 +0100

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -49,7 +49,7 @@ class TraductionView extends WidgetView {
 		super.draw();
 
 		this.bloc = HH.create("input"); //creer un champ texte. id = "ChampTexte" size = "39" type = "text"
-        this.bloc.setAttribute("id", "ChampTexte");
+     	        this.bloc.setAttribute("id", "ChampTexte");
 		this.bloc.setAttribute("size", "39");
 		this.bloc.setAttribute("type", "text");
 		this.stage.appendChild(this.bloc);
@@ -70,10 +70,10 @@ class TraductionView extends WidgetView {
 
 		this.stage.appendChild(this.langueBase); //mise en forme
         
-        this.langueTrad = HH.create("select"); //creer une liste deroulante pour la langue du mot a traduire. id = "langueTrad"
-        this.langueTrad.setAttribute("id", "langueTrad");
+                this.langueTrad = HH.create("select"); //creer une liste deroulante pour la langue du mot a traduire. id = "langueTrad"
+                this.langueTrad.setAttribute("id", "langueTrad");
         
-        this.francais2 = HH.create("option"); //contenue de la liste "langueTrad"
+                this.francais2 = HH.create("option"); //contenue de la liste "langueTrad"
 		this.francais2.innerHTML = "Francais";
 
 		this.langueTrad.appendChild(this.francais2); 
@@ -85,9 +85,9 @@ class TraductionView extends WidgetView {
 
 		this.stage.appendChild(this.langueTrad); //mise en forme
         
-        this.footer.innerHTML = "valider";  // mise en forme du footer permettant de valider les valeur et choix entrer.
+                this.footer.innerHTML = "valider";  // mise en forme du footer permettant de valider les valeur et choix entrer.
 		SS.style(this.footer, {"userSelect": "none", "cursor": "pointer"});
-		Events.on(this.footer, "click", event => this.mvc.controller.refreshClick());
+		Events.on(this.footer, "click", event => this.mvc.controller.valider());
 		this.stage.appendChild(this.footer);
         
 	}
@@ -111,16 +111,14 @@ class TraductionController extends WidgetController {
 		
 	}
     
-    refreshClick() {
-        this.tableauLangue = ["Francais","Anglais"]; //liste contenant les langues.
+    	valider() {
+        	this.tableauLangue = ["Francais","Anglais"]; //liste contenant les langues.
 		this.mot = document.getElementById("ChampTexte").value; // variable contenant la valeur contenue dans le champ texte.
-        this.base = document.getElementById("langueBase").selectIndex; 
-        this.baseChoix = langueBase.selectedIndex;  // variable qui contient le choix de la langue du mot
-        this.trad = document.getElementById("langueTrad").selectIndex;
-        this.tradChoix = langueTrad.selectedIndex; // variable qui contient le choix de langue dans lequel sera traduit le mot.
-        //alert("[" + this.mot + "]" + " " + this.tableauLangue[this.baseChoix] + " -->" + " " + this.tableauLangue[this.tradChoix] + " " );
-        
-        
+      	        this.base = document.getElementById("langueBase").selectIndex; 
+        	this.baseChoix = langueBase.selectedIndex;  // variable qui contient le choix de la langue du mot
+        	this.trad = document.getElementById("langueTrad").selectIndex;
+        	this.tradChoix = langueTrad.selectedIndex; // variable qui contient le choix de langue dans lequel sera traduit le mot.
+        	//alert("[" + this.mot + "]" + " " + this.tableauLangue[this.baseChoix] + " -->" + " " + this.tableauLangue[this.tradChoix] + " " );    
 	}
     
 	
