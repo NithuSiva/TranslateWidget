ca68066be1ff0f130bf3f8a719f2a2a8bc8ad04a
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-03-31 10:41:54 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -123,7 +123,7 @@ class TraductionController extends WidgetController {
         	//alert("[" + this.mot + "]" + " " + this.tableauLangue[this.baseChoix] + " -->" + " " + this.tableauLangue[this.tradChoix] + " " );
 		this.lien = "https://www.wordreference.com/" + this.tableauLangue[this.baseChoix] + this.tableauLangue[this.tradChoix] + "/" + this.mot;
 		//this.mvc.controller.load();
-		alert(this.lien);
+		alert(this.base);
 		
 	}
 		
@@ -131,11 +131,11 @@ class TraductionController extends WidgetController {
 	
 	async load() {
 		
-		let result = await this.mvc.main.dom("https://www.google.com/search?q=traduction&oq=traductio&aqs=chrome.0.69i59j69i57j0j69i61j69i60j69i61.1862j0j4&sourceid=chrome&ie=UTF-8"); // load web page
+		let result = await this.mvc.main.dom("https://www.developpez.com/"); // load web page
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
-		this.article = new xph().doc(dom).ctx(dom).craft('//*[@id="rso"]/div[2]/div/div[1]/div/div/div[1]/a/h3').firstResult; // find interesting things
+		this.article = new xph().doc(dom).ctx(dom).craft('//*[@id="colonneGauche"]/table/tbody/tr[1]/td/section/div/div[1]/div/div/h3/a').firstResult; // find interesting things
 		
 		this.mvc.view.update(article.textContent); 
 		
