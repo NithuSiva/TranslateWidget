2d08425ee1872824e0db77fdc18fd8075a0b6a34
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-04-02 11:49:18 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -136,7 +136,7 @@ class TraductionController extends WidgetController {
 		
 	async load() {
 		let link = await this.click;
-		let result = await this.mvc.main.dom("https://www.linguee.fr/francais-anglais/search?source=auto&query=maison"); // load web page
+		let result = await this.mvc.main.dom(link); // load web page
 		let domstr = _atob(result.response.dom); // decode result
 		let parser = new DOMParser(); // init dom parser
 		let dom = parser.parseFromString(domstr, "text/html"); // inject result
