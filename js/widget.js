e33d4e9c093da5a548e4dcf0f1a412ac08d9968f
NithuSiva <47365744+NithuSiva@users.noreply.github.com>
2019-04-03 15:31:15 +0200

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -15,8 +15,8 @@ class TraductionWidget extends Widget {
 	
 	async ready() {
 		super.ready();
-		
-		
+		await this.controller.valider();
+		this.controller.load();
 	}
 	
 }
@@ -130,7 +130,7 @@ class TraductionController extends WidgetController {
 		this.lien = "https://www.linguee.fr/" + this.tableauLangue[this.baseChoix] + "-" + this.tableauLangue[this.tradChoix] + "/search?source=auto&query=" + this.mot;
 		//return this.lien;
 		console.log(this.lien);
-		this.controller.load(this.lien);
+		
 	}
 		
 	async load(link) {
